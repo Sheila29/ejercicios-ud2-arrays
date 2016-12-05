31fa7d82a636b8b17c3f74475edffb1bee03e1fe
Sheilapsm <murgia85@gmail.com>
2016-12-05 21:51:15 +0100

    terminado

--- 07_tres_en_raya/src/com/company/Main.java
+++ 07_tres_en_raya/src/com/company/Main.java
@@ -1,159 +1,170 @@
 package com.company;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-    public static int[][] juego = new int[3][3];
-    public static int jugada;
-
-    public static boolean comprobarGanador(int jugada){
 
 
-        if((juego[0][0]==jugada) && (juego[0][1]==jugada) && (juego[0][2]==jugada)){
-            return true;
+
+    public static boolean comprobarGanador(int[][] t){
+
+
+        for (int i = 0; i < 2 ; i++) {
+
+
+            if ((t[0][0] == i) && (t[0][1] == i) && (t[0][2] == i)) {
+                return true;
+            }
+            if ((t[1][0] == i) && (t[1][1] == i) && (t[1][2] == i)) {
+                return true;
+            }
+            if ((t[2][0] == i) && (t[2][1] == i) && (t[2][2] == i)) {
+                return true;
+            }
+
+
+            for (int j = 0; j < 3; j++) {
+                if ((t[0][j] == i) && (t[1][j] == i) && (t[2][j] == i)) {
+                    return true;
+                }
+            }
+
+
+
+            if ((t[0][0] == i) && (t[1][1] == i) && (t[2][2] == i)) {
+                return true;
+            }
+            if ((t[0][2] == i) && (t[1][1] == i) && (t[2][0] == i)) {
+                return true;
+            }
+
+
         }
-        if((juego[1][0]==jugada) && (juego[1][1]==jugada) && (juego[1][2]==jugada)){
-            return true;
-        }
-        if((juego[2][0]==jugada) && (juego[2][1]==jugada) && (juego[2][2]==jugada)){
-            return true;
-        }
-        if((juego[0][0]==jugada) && (juego[2][0]==jugada) && (juego[3][0]==jugada)){
-            return true;
-        }
-        if((juego[0][1]==jugada) && (juego[1][1]==jugada) && (juego[2][1]==jugada)){
-            return true;
-        }
-        if((juego[0][2]==jugada) && (juego[1][2]==jugada) && (juego[2][2]==jugada)){
-            return true;
-        }
-        if((juego[0][0]==jugada) && (juego[1][1]==jugada) && (juego[2][2]==jugada)){
-            return true;
-        }
-        if((juego[0][2]==jugada) && (juego[1][1]==jugada) && (juego[2][0]==jugada)){
-            return true;
-        }
+
         return false;
     }
 
 
 
-    public static void visualizarTablero(){
 
-        int filas_tablero;
-        int columnas_tablero;
+    public static void visualizarTablero(int[][] t) {
 
-        System.out.println("-------------");
-        for(int i = 0; i < juego.length;i++){
-            for(int j=0; j< juego.length;j++){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
 
-                if(juego[i][j]==0){
-                    System.out.print("| " + juego[i][j] + " ");
-                }
-                if (juego[i][j]==1)
-                {
-                    System.out.print("| " + "X" + " ");
 
-                }
-                if (juego[i][j]==3)
-                {
-                    System.out.print("|" + "  ");
+                switch (t[i][j]) {
+                    case 0:
+                        System.out.println(" O ");
+                        break;
+                    case 1:
+                        System.out.println(" X ");
+                        break;
+                    case 7:
+                        System.out.println(" . ");
+                        break;
+
 
                 }
+
             }
-            System.out.print("|");
-            System.out.println("");
+
+            System.out.println();
+
+
         }
-        System.out.println("-------------");
+
 
     }
 
-    public static void limpiarTablero(){
-        for(int i = 0; i < juego.length;i++){
-            for(int j = 0; j < juego.length;j++){
-                juego[i][j]=3;
-            }
-        }
-    }
 
 
-        public static void comenzarJuego(int fila, int columna)  {
-
-            boolean partidaTerminada = false;
-
-            if (juego[fila][columna] == 3) {
-                boolean ganador;
-                juego[fila][columna] = jugada;
-                visualizarTablero();
-                ganador = comprobarGanador(jugada);
-
-                if (ganador) {
-                    System.out.println("Ha ganado " + jugada);
-                    partidaTerminada = true;
-                    limpiarTablero();
-
-                } else {
-                    int empate = 0;
-                    for (int i = 0; i <= 2; i++) {
-                        for (int j = 0; j <= 2; j++) {
-                            if ((juego[i][j] == 0) || (juego[i][j] == 1)) {
-                                empate = empate + 1;
-                            }
-                        }
-                    }
-                    if (empate == 9) {
-                        System.out.println("Se ha empatado");
-                        partidaTerminada = true;
-
-                    }
-                    if (jugada == 1) {
-                        jugada = 0;
-                    } else {
-                        if (jugada == 0) {
-                            jugada = 1;
-                        }
 
 
-                    }
 
-                }
-            }
-        }
 
 
         public static void main(String[] args) throws IOException {
 
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int[][] t = new int[3][3];
 
-            System.out.println("¿Cómo quieres jugar X/0?");
-            String juego = br.readLine();
-
-            int jugada = 0;
-            if(juego.equalsIgnoreCase("0")){
-                jugada = 0;
-            }
-            if(juego.equalsIgnoreCase("X")){
-                jugada = 1;
-            }
-
-            if(juego.equalsIgnoreCase("X") || (juego.equalsIgnoreCase("0"))){
-                System.out.println("Comienza el 0");
-                System.out.println("");
+            // inicializar las casillas vacías
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    t[i][j] = 7;
 
 
-
+                }
             }
 
-                    System.out.println("Introduce coordenadas de posición: (0,0) ");
-                    System.out.println("Introduce la fila");
-                    int fila= Integer.parseInt(br.readLine());
 
-                    System.out.println("Introduce la columna");
-                    int columna= Integer.parseInt(br.readLine());
+            // mostrar el tablero
+            visualizarTablero(t);
+
+            // ver a quien le toca jugar
+            int turno = 0;
+
+            int tiradas = 0;
+
+            do {
+                // el usuario elija casilla
+                System.out.print("fila (0-2): ");
+                int fila = Integer.parseInt(br.readLine());
+
+                System.out.println("columna(0-2): ");
+                int columna = Integer.parseInt(br.readLine());
+
+                // ver si la tirada es válida y actualizar tablero
+                if (t[fila][columna] == 7) {
+                    // válida
+                    t[fila][columna] = turno;
+
+                    // contabilizar tirada
+                    tiradas +=1;
+
+                    // cambio turno
+                    if (turno == 0) {
+                        turno = 1;
+                    } else {
+                        turno = 0;
+                    }
+
+                    // mostrar tablero
+                    visualizarTablero(t);
+
+
+                } else {
+                    System.err.println("Casilla ocupada...");
+                }
+
+            }while(tiradas < 9 && !comprobarGanador(t));
+
+            if(tiradas < 9){
+                System.out.println("enhorabuena!! has ganado");
+            }else{
+                System.out.println("empate");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
